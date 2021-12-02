f4477b22d3624674bf3adca9eba9379b931c8b03
anna bozio <anna.bozio@gmail.com>
2021-12-02 08:11:54 +0100

    refacto

--- test2.rb
+++ test2.rb
@@ -3,41 +3,32 @@ require 'uri'
 url = 'test2.txt'
 file = File.open(url)
 file_data = file.readlines.map(&:chomp)
-counts = Hash.new(0)
-file_data.each { |name| counts[name] += 1 }
+array = file_data.map {|x| x.split(" ")}
 def first(array)
   horizontal = 0
-  vertical = 0
-  array.each do |key, value|
-    if key[0] == "f"
-      (key[-1].to_i * value.to_i)
-      horizontal += (key[-1].to_i * value.to_i)
-    elsif key[0] == "d"
-
-      vertical += (key[-1].to_i * value.to_i)
-    elsif key[0] == "u"
-      (key[-1].to_i * value.to_i)
-      vertical -= (key[-1].to_i * value.to_i)
+  depth = 0
+  array.each do |x|
+    if x.first[0] == "d"
+      depth += x.last.to_i
+    elsif x.first[0] == "u"
+      depth -= x.last.to_i
+    elsif x.first[0] == "f"
+      horizontal += x.last.to_i
     end
-
   end
-  p horizontal * vertical
+  p horizontal * depth
 end
-first(counts)
-array = file_data.map {|x| x.split(" ")}
+first(array)
 def second (array)
   aim = 0
   horizontal = 0
   depth = 0
   array.each do |x|
-  if x.first[0] == "d"
-
-    aim += x.last.to_i
-  elsif x.first[0] == "u"
-    aim -= x.last.to_i
-  elsif x.first[0] == "f"
-    p aim
-    p x.last.to_i
+    if x.first[0] == "d"
+      aim += x.last.to_i
+    elsif x.first[0] == "u"
+      aim -= x.last.to_i
+    elsif x.first[0] == "f"
       depth += x.last.to_i
       horizontal += (x.last.to_i * aim)
     end
