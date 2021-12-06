7fed301b2b7bcfbb56e96621aec96b08b0d44c97
anna bozio <anna.bozio@gmail.com>
2021-12-06 01:27:40 +0100

    test4 refacto

--- test4.rb
+++ test4.rb
@@ -5,7 +5,7 @@ require 'uri'
 url = 'test4.txt'
 file = File.open(url)
 file_data = file.readlines.map(&:chomp)
-random_umbers = file_data.first.split(',')
+random_numbers = file_data.first.split(',')
 file_data.slice!(0)
 html = []
 file_data.each { |x| html << x if x != '' }
@@ -19,28 +19,60 @@ def verification_line(array)
   array.any? { |line| line == %w[X X X X X] }
 end
 
-def verification_collumn(array, index)
-  array.all? { |line| line[index] == 'X' }
+def verification_collumn(array)
+  array.transpose.any? { |line| line == %w[X X X X X] }
 end
 
-all_grid_array = all_grids.map { |grid| grid.map { |line| line.split(' ') } }
-random_umbers.each do |num|
-  all_grid_array.each do |grid|
-    grid.each do |line|
-      include_in_line_and_replace?(num, line)
-      next unless verification_line(grid) || verification_collumn(grid,
-                                                                  0) || verification_collumn(grid,
-                                                                                             1) || verification_collumn(grid,
-                                                                                                                        2) || verification_collumn(
-                                                                                                                          grid, 3
-                                                                                                                        ) || verification_collumn(
-                                                                                                                          grid, 4
-                                                                                                                        )
+def split_in_array(grid)
+  grid.map { |grid| grid.map { |line| line.split(' ') } }
+end
 
-      left_numbers = 0
-      grid.each { |line| line.each { |x| left_numbers += x.to_i if x != 'X' } }
-      p left_numbers * num.to_i
-      return
+def part_one(grid, numbers)
+  all_grid_array = split_in_array(grid)
+  numbers.each do |num|
+    all_grid_array.each do |grid|
+      grid.each do |line|
+        include_in_line_and_replace?(num, line)
+        next unless verification_line(grid) || verification_collumn(grid)
+        left_numbers = 0
+        grid.each { |line| line.each { |x| left_numbers += x.to_i if x != 'X' } }
+        p left_numbers * num.to_i
+        return
+      end
     end
   end
 end
+
+part_one(all_grids, random_numbers)
+
+def part_two(grid, numbers)
+  all_grid_array = split_in_array(grid)
+  find_last_winner(numbers,all_grid_array)
+end
+
+def find_numbers_not_used(grid, num)
+  left_numbers = 0
+  grid.each { |line| line.each { |x| left_numbers += x.to_i if x != 'X' } }
+  p left_numbers * num.to_i
+end
+
+def find_last_winner(numbers,grid_array)
+  numbers.each do |num|
+    grid_array.each do |grid|
+      grid.each do |line|
+        include_in_line_and_replace?(num, line)
+        if verification_line(grid) || verification_collumn(grid)
+          grid_array.slice!(grid_array.index(grid))
+          if grid_array.length == 0
+            return find_numbers_not_used(grid, num)
+          end
+          find_last_winner(numbers,grid_array)
+        else
+          next
+        end
+        return
+      end
+    end
+  end
+end
+part_two(all_grids,random_numbers)
