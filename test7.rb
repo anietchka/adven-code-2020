1faaae1ccea3b63c78048bdf1b61e8d012a17177
anna bozio <anna.bozio@gmail.com>
2021-12-07 18:45:12 +0100

    test7 part 2 done

--- test7.rb
+++ test7.rb
@@ -13,7 +13,6 @@ end
 
 def part_one(array)
   count = 0
-  median(array)
   array.each do |num|
     count += num > median(array) ? num - median(array) : median(array) - num
   end
@@ -40,12 +39,59 @@ def increase(first_num, last_num)
 end
 
 def part_two(array)
-  count = 0
-  p mean = mean(array).to_i
+  count_mean = 0
+  count_mean_min = 0
+  count_mean_max = 0
+  mean = mean(array).round
   array.each do |num|
-    count += increase(num, mean)
+    count_mean += increase(num, mean)
+    count_mean_min += increase(num, mean - 1)
+    count_mean_max += increase(num, mean - 2)
   end
-  count
+  [count_mean, count_mean_min, count_mean_max].min
 end
 
 p part_two(input_integer)
+# irb
+# require 'mon_fichier'
+# App.new("input.txt").run
+
+class App
+
+  def initialize(file)
+    @crew = Open.file(file) # parse
+  end
+
+  def run
+    CrabCrew.new(@crew).least_fuel
+  end
+end
+
+class CrabCrew
+  def initialize(crew)
+    @crew = crew
+  end
+
+  def least_fuel
+    position = {}
+    (min..max).each do |p|
+      position[p] = @crew.map do |crab_position|
+        difference = (p - crab_position).abs
+        difference*(difference-1)/2
+      end.sum
+    end
+
+    return position.values.min
+  end
+
+  private
+
+  def min
+    @crew.min
+  end
+
+  def max
+    @crew.max
+  end
+
+end
